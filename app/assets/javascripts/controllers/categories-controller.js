8afbaff3ac9b5625781762fb397133654dbc8918
Alex Beznos <beznosa@yahoo.com>
2014-10-06 20:11:44 +0300

    Fix categories creation

--- app/assets/javascripts/controllers/categories-controller.js
+++ app/assets/javascripts/controllers/categories-controller.js
@@ -6,22 +6,33 @@ Store.CategoriesController = Ember.ArrayController.extend({
 			this.toggleProperty('isForm')
 		},
 		createCategory: function() {
-			var controller = this,
-				category = this.store.createRecord('category', {
-					name: this.get('newName'),
-					alias: this.get('newAlias'),
-					position: this.get('newPosition'),
-					menu: this.get('newMenu'),
-					status: this.get('newStatus'),
-					parent_category_id: this.get('parentID')
-				});
+			if(this.get('newName') && this.get('newAlias')) {
+				var controller = this,
+					category = this.store.createRecord('category', {
+						name: this.get('newName'),
+						alias: this.get('newAlias'),
+						position: this.get('newPosition'),
+						menu: this.get('newMenu'),
+						status: this.get('newStatus'),
+						parent_category_id: this.get('parentID')
+					});
 				category.save().then(function() {
 					controller.send('openForm');
-					console.log(controller.get('parentID'));
 					if(controller.get('parentID') != undefined) {
 						location.reload()
+					} else {
+						controller.set('newName', '');
+						controller.set('newAlias', '');
+						controller.set('newPosition', '');
+						controller.set('newMenu', '');
+						controller.set('newStatus', false);
 					}
 				})
+			} else {
+				$('#createCategory').children('.alert').remove('.alert');
+				$('#createCategory').prepend("<div class='alert alert-danger' role='alert'>Неможливо створити новий документ без назви!</div>");
+			}
+
 		}
 	}
 });
