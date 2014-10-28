f99c6811c87671f3c51db91d18e64e1ab807d158
Alex Beznos <beznosa@yahoo.com>
2014-10-28 10:26:43 +0200

    Fix categories output

--- app/assets/javascripts/admin/controllers/subcategory-controller.js
+++ app/assets/javascripts/admin/controllers/subcategory-controller.js
@@ -1,4 +1,8 @@
 Admin.SubcategoryController = Ember.ObjectController.extend({
+	parent_category: Ember.computed(function() {
+		var id = this.get('parent_category_id');
+		return this.store.find('category', id);
+	}),
 	actions: {
 		destroy: function() {
 			var model = this.get('model');
