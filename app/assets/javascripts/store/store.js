3f04deba615c222154a64317ff68ee07c8de7d3d
Alex Beznos <beznosa@yahoo.com>
2014-10-28 20:04:34 +0200

    Fix namespace in store

--- app/assets/javascripts/store/store.js
+++ app/assets/javascripts/store/store.js
@@ -17,7 +17,7 @@ Store.Store = DS.Store.extend({
 // Override the default adapter with the `DS.ActiveModelAdapter` which
 // is built to work nicely with the ActiveModel::Serializers gem.
 DS.RESTAdapter.reopen({
-  namespace: 'api'
+  namespace: 'api/administration'
 });
 
 
